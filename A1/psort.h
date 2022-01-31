e4e989b464755376b3255f37424cde1f935d2030
SidharthAgarwal18 <sidharthagarwal2001@gmail.com>
2022-01-31 20:02:57 +0530

    final

--- A1/psort.h
+++ A1/psort.h
@@ -2,4 +2,6 @@
 
 #include <stdint.h>
 
+void merge(uint32_t *arr1,uint32_t size1,uint32_t *arr2,uint32_t size2,uint32_t* newarr);
+void sequentialSort(uint32_t *arr,uint32_t size);
 void ParallelSort(uint32_t *data, uint32_t n, int p);
